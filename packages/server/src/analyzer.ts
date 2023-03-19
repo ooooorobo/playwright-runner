30222e1b592f982f60a28b938fdf6ec155cae35f
조예진 <orobos654@gmail.com>
2023-03-19 22:57:00 +0900

    fix: get all tests
    특정 디렉토리 하위에 있는 경우 못 가져오는 것 수정

--- packages/server/src/analyzer.ts
+++ packages/server/src/analyzer.ts
@@ -1,22 +1,55 @@
+import * as process from "process";
 import * as fs from "fs";
 import path from "path";
-import * as process from "process";
 
-export const analyzeTestFiles = (): Promise<boolean> => {
+const readFromDirectory = async (dirPath: string): Promise<string[]> => {
     return new Promise((resolve, reject) => {
-        const dirPath = path.join(process.cwd(), 'packages/e2e');
-        const files = fs.readdirSync(dirPath);
-        const filtered = files.filter(name => /\**\.(spec|test)\.ts/.test(name));
+        fs.readdir(dirPath, (err, files) => {
+            if (err) reject(err);
+            resolve(files);
+        })
+    })
+}
 
-        const testNames = filtered
-            .map((name) => {
-                const fileContents = fs.readFileSync(path.join(dirPath, name), 'utf8') ?? '';
-                return (fileContents.match(/test\('(.+?)',/g) ?? []).map((title) => {
-                    return title.slice(6, -2);
-                })
+const isFile = async (filenameWithPath: string): Promise<boolean> => {
+    return new Promise((resolve, reject) => {
+        fs.stat(filenameWithPath, (err, stats) => {
+            if (err) reject(err);
+            resolve(stats.isFile());
+        })
+    })
+}
+
+export const analyzeTestFiles = async (): Promise<boolean> => {
+    const dirPath = path.join(process.cwd(), 'packages/e2e');
+    const names = (await readFromDirectory(dirPath)).map(name => ['', name]);
+    const fileNames = [];
+    for (const [dir, name] of names) {
+        const joined = path.join(dirPath, dir, name);
+        if (await isFile(joined)) {
+            fileNames.push([dir, name].join('/'))
+        } else {
+            names
+                .push(
+                    ...(await readFromDirectory(joined))
+                        .map((foundName) => [[dir, name].join('/'), foundName])
+                )
+        }
+    }
+
+    const testFileNames = fileNames.filter(name => /\**\.(spec|test)\.ts/.test(name));
+
+    const testNames = testFileNames
+        .map((name) => {
+            const fileContents = fs.readFileSync(path.join(dirPath, name), 'utf8') ?? '';
+            return (fileContents.match(/test\('(.+?)',/g) ?? []).map((title) => {
+                return title.slice(6, -2);
             })
-            .flat()
-            .filter(Boolean);
+        })
+        .flat()
+        .filter(Boolean);
+
+    return new Promise((resolve, reject) => {
 
         const ws = fs.createWriteStream(path.join(process.cwd(), 'packages/server/data/tests.json'));
         ws.on('close', () => resolve(true))
